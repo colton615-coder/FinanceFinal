4d83cf8b21910ad9c308f5776d56adebe143f5d7
colton615-coder <colton615@gmail.com>
2025-09-09 09:35:22 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'budget-pwa-v1';
+const CACHE = 'budget-pwa-v8';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','./assets/apple-touch-icon.png'];
 
 self.addEventListener('install', (event) => {
